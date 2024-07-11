756f899d675d464483a3e2a4600fa49eb4d184b9
Michal Wysokinski <mwysokinski@google.com>
2024-07-11 17:45:46 +0200

    debug: test finished code branches

--- pkg/controller/jobframework/reconciler.go
+++ pkg/controller/jobframework/reconciler.go
@@ -334,6 +334,7 @@ func (r *JobReconciler) ReconcileGenericJob(ctx context.Context, req ctrl.Reques
 			return ctrl.Result{}, err
 		}
 
+		log.V(1).Info("MW-DEBUG: Workload with status condition finished is finished")
 		r.record.Eventf(object, corev1.EventTypeNormal, ReasonFinishedWorkload,
 			"Workload '%s' is declared finished", workload.Key(wl))
 		return ctrl.Result{}, workload.RemoveFinalizer(ctx, r.client, wl)
@@ -364,6 +365,7 @@ func (r *JobReconciler) ReconcileGenericJob(ctx context.Context, req ctrl.Reques
 			if err != nil && !apierrors.IsNotFound(err) {
 				return ctrl.Result{}, err
 			}
+			log.V(1).Info("MW-DEBUG: Workload with standard reconciliation is finished")
 			r.record.Eventf(object, corev1.EventTypeNormal, ReasonFinishedWorkload,
 				"Workload '%s' is declared finished", workload.Key(wl))
 		}
